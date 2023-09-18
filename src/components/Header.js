07868bd596ec558fd174fe4fb25aec829d378a18
bssbrit <brunosabrito2@gmail.com>
2023-09-18 16:24:52 -0300

    made the basic envyroment

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactComponent as Logo } from "../icons/icon-moon.svg";
+const Header = () => {
+  return (
+    <div>
+      <h1>TODO</h1>
+      <div>
+        <Logo />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
